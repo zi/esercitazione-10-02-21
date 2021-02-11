708036d1cfc0e667f4cf2e7edcb23c89c1e725d9
Michelangelo Morabito <michimorabito@gmail.com>
2021-02-11 18:12:11 +0100

    my 1st and 2nd exercise 10february

--- esercit23miki.js
+++ esercit23miki.js
@@ -1,44 +1,47 @@
-const TOAST = document.querySelector(".toast");
-const POPULAR_MOVIES = document.querySelector("#popularMovies");
+const TOAST = document.querySelector('.toast')
+const POPULAR_MOVIES = document.querySelector('#popularMovies')
+const TOPRATED_MOVIES = document.querySelector('#topRatedMovies')
+const POPULAR_TVSERIES = document.querySelector('#popularTVseries')
 
 const state = {
   config: {
-    api_key: "LA VOSTRA API KEY",
-    base_url: "https://api.themoviedb.org/3",
-    images: null
+    api_key: '4efd416bce1421bc186bc4d7599fbd04',
+    base_url: 'https://api.themoviedb.org/3',
+    images: null,
   },
-  movies: null
-};
+  movies: { popular: null, toprated: null },
+  tvseries: { popular_tv: null },
+}
 
 /**
  * Utilities
  */
 function getUrl(pathName) {
-  const { api_key, base_url } = state.config;
+  const { api_key, base_url } = state.config
 
-  return `${base_url}${pathName}?api_key=${api_key}`;
+  return `${base_url}${pathName}?api_key=${api_key}`
 }
 
 function getImageUrl(imgPath) {
   // const { secure_base_url, backdrop_sizes } = state.config.images
 
-  const secure_base_url = state.config.images.secure_base_url;
-  const backdrop_sizes = state.config.images.backdrop_sizes;
+  const secure_base_url = state.config.images.secure_base_url
+  const backdrop_sizes = state.config.images.backdrop_sizes
 
-  return `${secure_base_url}${backdrop_sizes[0]}${imgPath}`;
+  return `${secure_base_url}${backdrop_sizes[0]}${imgPath}`
 }
 
 async function getData(url) {
   try {
-    const response = await fetch(url);
-    const result = await response.json();
+    const response = await fetch(url)
+    const result = await response.json()
 
     if (!response.ok) {
-      throw result;
+      throw result
     }
-    return result;
+    return result
   } catch (errorMessage) {
-    console.log(errorMessage);
+    console.log(errorMessage)
   }
 }
 
@@ -52,11 +55,11 @@ async function getData(url) {
  * @link https://developers.themoviedb.org/3/authentication/create-guest-session
  */
 async function getGuestSession() {
-  const guestSessionUrl = getUrl("/authentication/guest_session/new");
+  const guestSessionUrl = getUrl('/authentication/guest_session/new')
 
-  const result = await getData(guestSessionUrl);
+  const result = await getData(guestSessionUrl)
 
-  return result;
+  return result
 }
 
 /**
@@ -65,14 +68,14 @@ async function getGuestSession() {
  * @link https://developers.themoviedb.org/3/configuration/get-api-configuration
  */
 async function getConfiguration() {
-  const configurationUrl = getUrl("/configuration");
+  const configurationUrl = getUrl('/configuration')
 
-  const result = await getData(configurationUrl);
+  const result = await getData(configurationUrl)
 
   // aggiorniamo il nostro state interno con i dati ricevuti
-  state.config.images = result.images;
+  state.config.images = result.images
 
-  return result;
+  return result
 }
 
 /**
@@ -81,15 +84,34 @@ async function getConfiguration() {
  * @link https://developers.themoviedb.org/3/movies/get-popular-movies
  */
 async function getPopularMovies() {
-  const popularMoviesURL = getUrl("/movie/popular");
+  const popularMoviesURL = getUrl('/movie/popular')
 
-  const rawResponse = await getData(popularMoviesURL);
+  const rawResponse = await getData(popularMoviesURL)
 
-  state.movies = rawResponse.results;
+  state.movies.popular = rawResponse.results
 
-  return rawResponse;
+  return rawResponse
 }
 
+async function getTopRated() {
+  const topRatedMoviesURL = getUrl('/movie/top_rated')
+
+  const rawResponse = await getData(topRatedMoviesURL)
+
+  state.movies.toprated = rawResponse.results
+
+  return rawResponse
+}
+
+async function getTVseriesPopular() {
+  const tvSeriesPopularURL = getUrl('/tv/popular')
+
+  const rawResponse = await getData(tvSeriesPopularURL)
+
+  state.tvseries.popular_tv = rawResponse.results
+
+  return rawResponse
+}
 /**
  * gestisce la sessione guest dell'utente
  *
@@ -102,33 +124,33 @@ async function getPopularMovies() {
  */
 async function handleSession() {
   // ottiene il dato da localStorage
-  const sessionData = localStorage.getItem("mdb_session");
+  const sessionData = localStorage.getItem('mdb_session')
 
   // se sessionData è undefined
   if (!sessionData) {
     // crea una nuova sessione
-    const newSessionData = await getGuestSession();
+    const newSessionData = await getGuestSession()
 
     // se la chiamata getGuestSession ritorna un valore
     if (newSessionData) {
       // trasforma in stringa l'oggetto (localStorage può avere solo stringhe)
-      const sessionDataString = JSON.stringify(newSessionData);
+      const sessionDataString = JSON.stringify(newSessionData)
 
       // aggiunge il valore nel localStorage
-      localStorage.setItem("mdb_session", sessionDataString);
+      localStorage.setItem('mdb_session', sessionDataString)
 
       // mostra il toastBaner per dare un feedback alll'utente
-      showToast("Hey! Adesso sei registrato come guest");
+      showToast('Hey! Adesso sei registrato come guest')
 
-      return true;
+      return true
     }
 
-    return false;
+    return false
   } else {
     // se sessionData ha un valore
 
     // trasforma la stringa ottenuta da localSotarge in oggetto o variabile primitiva
-    const parsedSessionData = JSON.parse(sessionData);
+    const parsedSessionData = JSON.parse(sessionData)
 
     /**
      * controlliamo che la sessione non sia scacduta
@@ -144,22 +166,22 @@ async function handleSession() {
      * (è uno standard per avere una costante di riferimento)
      *
      */
-    const expiresDate = new Date(parsedSessionData.expires_at).getTime();
-    const nowDate = new Date().getTime();
+    const expiresDate = new Date(parsedSessionData.expires_at).getTime()
+    const nowDate = new Date().getTime()
 
     // se expiresDate in millisecondi è inferiore
     // a nowDate in millisecondi allora la sessione è scaduta
     if (expiresDate < nowDate) {
       // rimuoviamo i dati della sessione del localStorage
-      localStorage.removeItem("mdb_session");
+      localStorage.removeItem('mdb_session')
 
       // chiamiamo la funzione stessa per gestire la
       // creazione di una nuova sessione e l'inserimento nel localStorage
-      await handleSession();
+      await handleSession()
 
-      return true;
+      return true
     }
-    return true;
+    return true
   }
 }
 
@@ -168,34 +190,34 @@ async function handleSession() {
  * che gli viene passato come parametro
  */
 function showToast(text) {
-  TOAST.textContent = text;
-  TOAST.classList.toggle("toast__is-hidden");
+  TOAST.textContent = text
+  TOAST.classList.toggle('toast__is-hidden')
 
   setTimeout(() => {
-    TOAST.classList.toggle("toast__is-hidden");
-  }, 4000);
+    TOAST.classList.toggle('toast__is-hidden')
+  }, 4000)
 }
 
 /**
  * Crea una card per i film / serie tv
  */
 function getMovieCard(imgURL, title) {
-  const cardWrap = document.createElement("div");
-  const coverImg = document.createElement("img");
+  const cardWrap = document.createElement('div')
+  const coverImg = document.createElement('img')
 
-  const textWrap = document.createElement("div");
-  const text = document.createElement("h3");
+  const textWrap = document.createElement('div')
+  const text = document.createElement('h3')
 
-  cardWrap.classList.add("card");
-  textWrap.classList.add("card__title_wrap");
+  cardWrap.classList.add('card')
+  textWrap.classList.add('card__title_wrap')
 
-  text.textContent = title;
-  coverImg.src = imgURL;
+  text.textContent = title
+  coverImg.src = imgURL
 
-  textWrap.appendChild(text);
-  cardWrap.append(coverImg, textWrap);
+  textWrap.appendChild(text)
+  cardWrap.append(coverImg, textWrap)
 
-  return cardWrap;
+  return cardWrap
 }
 
 /**
@@ -206,12 +228,12 @@ function getMovieCard(imgURL, title) {
 function renderCarousel(list, sectionNode) {
   list.forEach((item) => {
     // ottiene la url dell'immagine completa
-    const imgURL = getImageUrl(item.backdrop_path);
+    const imgURL = getImageUrl(item.backdrop_path)
 
-    const movieCard = getMovieCard(imgURL, item.title);
+    const movieCard = getMovieCard(imgURL, item.title || item.name)
 
-    sectionNode.appendChild(movieCard);
-  });
+    sectionNode.appendChild(movieCard)
+  })
 }
 
 /**
@@ -219,12 +241,18 @@ function renderCarousel(list, sectionNode) {
  * e quando li ha ottenuti renderizza il carosello dei film popolari
  */
 function handleHTMLMounted() {
-  Promise.all([handleSession(), getConfiguration(), getPopularMovies()]).then(
-    () => {
-      // ci permette di lavorare con i dati ottenuti dall'esterno
-      renderCarousel(state.movies, POPULAR_MOVIES);
-    }
-  );
+  Promise.all([
+    handleSession(),
+    getConfiguration(),
+    getPopularMovies(),
+    getTopRated(),
+    getTVseriesPopular(),
+  ]).then(() => {
+    // ci permette di lavorare con i dati ottenuti dall'esterno
+    renderCarousel(state.movies.popular, POPULAR_MOVIES)
+    renderCarousel(state.movies.toprated, TOPRATED_MOVIES)
+    renderCarousel(state.tvseries.popular_tv, POPULAR_TVSERIES)
+  })
 }
 
 /**
@@ -235,6 +263,6 @@ function handleHTMLMounted() {
  *
  * rimuove il listenr una volta terminata l'operazione con {once: true}
  */
-document.addEventListener("DOMContentLoaded", handleHTMLMounted, {
-  once: true
-});
+document.addEventListener('DOMContentLoaded', handleHTMLMounted, {
+  once: true,
+})
